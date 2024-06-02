a9c75697d9b45e68d73fd981bb94280068b90dc6
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2024-06-02 19:12:01 -0300

    adjusts

--- src/models/entities/agency.py
+++ src/models/entities/agency.py
@@ -1,10 +1,65 @@
-class Agency:
+from src.models.tables import Account
+
+
+if not __name__ == "__main__":
+    from src.models.entities.bank import Bank
+    from src.models.entities.base import Base
+else:
+    from base import Base
+    from bank import Bank
+
+class Agency(Base):
     def __init__(self, 
                  id: int, 
                  name: str,
                  number: str,
-                 bank: object):
+                 bank: Bank):
         self._id = id
         self._name = name
         self._number = number
         self._bank = bank
+        
+        self.accounts = [] # list of accounts that are associated with the account
+        
+    @property
+    def id(self) -> int:
+        return self._id
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def number(self) -> str:
+        return self._number
+    
+    @property
+    def bank(self) -> object:
+        return self._bank
+    
+    @name.setter
+    def name(self, name: str) -> None:
+        self._name = name
+        
+    @number.setter
+    def number(self, number: str) -> None:
+        self._number = number
+        
+    def add_account(self, new_account: Account) -> bool:
+        
+        if isinstance(new_account, Account):
+            if not any(account.id == new_account.id for account in self.accounts): # check if account already exists, if not exists, app
+                self.accounts.append(new_account)
+                return True
+            
+        return False
+    
+    def remove_account(self, id: int) -> bool:
+        if len(self.accounts) != 0:
+            for account in self.accounts:
+                if account.id == id:
+                    del account
+                    return True
+        
+        return False
+    
